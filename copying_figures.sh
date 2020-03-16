732a035ef23f10dfd26afeeacf2f486be93ffd63
Shannon Bengtson <z5145948@ccrc189.ccrc.unsw.edu.au>
2020-03-16 11:28:16 +1100

    Initial commit

---
+++ copying_figures.sh
@@ -0,0 +1,5 @@
+cp Figures/* /home/z5145948/Dropbox/Apps/Overleaf/Interglacial_to_Holocene/Figures/.
+
+cp Figures/* /home/z5145948/Dropbox/Apps/Overleaf/The_deglaciation/Figures/.
+
+cp Figures/Regional_comparison_presentation_1.png Figures/Spatial_plot_presentation*  Figures/global_volume_weights_presentation.png Figures/Atlantic*png Figures/d13C*png /home/z5145948/Dropbox/Apps/Overleaf/third_year_presentation/Figures/.
